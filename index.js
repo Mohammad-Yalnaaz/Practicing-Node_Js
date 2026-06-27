143a33a758cac8966a723b226592a4a9e394474c
Mohammad-Yalnaaz <mohdyalnaaz@gmail.com>
2026-06-27 16:08:24 +0530

    First Route Complete

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const users = require("./MOCK_DATA.json");
+
 const app = express();
 const port = 3000;
 
+
+
 //Routes
+app.get('/users', (req,res) => {
+    return res.json(users);
+})
 
 
 app.listen(port, () => console.log('Server Started on port ' + port));
