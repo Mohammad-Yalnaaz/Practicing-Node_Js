5dbc070f86eab24901b43a20885ffef6b3c999ff
Mohammad-Yalnaaz <mohdyalnaaz@gmail.com>
2026-06-27 16:24:50 +0530

    created route for html and json

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ const port = 3000;
 
 
 //Routes
-app.get('/users', (req,res) => {
+app.get('/api/users', (req,res) => {
     return res.json(users);
 })
-
+app.get('/users', (req,res) => {
+    const html = `
+    <ul>
+    ${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join('')}
+    </ul>
+    `;
+    res.send(html);
+})
 
 app.listen(port, () => console.log('Server Started on port ' + port));
